a5eb4db8aaed0809e033fabead818f0dfaa9062a
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2026-02-28 15:31:27 +0100

    Make linting ocmplain less

--- src/lib/components/MetaTags.svelte
+++ src/lib/components/MetaTags.svelte
@@ -1,29 +1,6 @@
-<script lang="ts" module>
+<script lang="ts">
     import { resolve } from "$app/paths";
-
     export const prerender = true;
-
-    export const webManifest = {
-        name: "shIT",
-        short_name: "shIT",
-        icons: [
-            {
-                src: "/web-app-manifest-192x192.png",
-                sizes: "192x192",
-                type: "image/png",
-                purpose: "maskable",
-            },
-            {
-                src: "/web-app-manifest-512x512.png",
-                sizes: "512x512",
-                type: "image/png",
-                purpose: "maskable",
-            },
-        ],
-        theme_color: "#443355",
-        background_color: "#0e0e18",
-        display: "standalone",
-    };
 </script>
 
 <title>shIT</title>
@@ -38,10 +15,11 @@
 <meta name="theme-color" content="#443355" media="(prefers-color-scheme: dark)" />
 <meta name="theme-color" content="#f4f4fc" media="(prefers-color-scheme: light)" />
 
-<link rel="icon" type="image/png" href={resolve("/favicon-96x96.png")} />
-<link rel="icon" type="image/svg+xml" href={resolve("/favicon.svg")} />
-<link rel="shortcut icon" href={resolve("/favicon.ico")} />
-<link rel="apple-touch-icon" sizes="180x180" href={resolve("/apple-touch-icon.png")} />
+<link rel="icon" type="image/png" href="/favicon-96x96.png" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 <meta name="apple-mobile-web-app-title" content="shIT" />
 
+<!-- Resolved becasue it is a real api endpoint -->
 <link rel="manifest" href={resolve("/site.webmanifest")} />
--- src/routes/site.webmanifest/+server.ts
+++ src/routes/site.webmanifest/+server.ts
@@ -1,6 +1,26 @@
-import { webManifest } from "$lib/components/MetaTags.svelte";
-
 export const prerender = true;
+
+const webManifest = {
+    name: "shIT",
+    short_name: "shIT",
+    icons: [
+        {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+        },
+        {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+        },
+    ],
+    theme_color: "#443355",
+    background_color: "#0e0e18",
+    display: "standalone",
+} as const;
 
 export async function GET() {
     return new Response(JSON.stringify(webManifest), {
