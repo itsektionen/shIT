55679d70af8e0de35c088bf7f5844f5449930bf7
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2026-03-16 17:57:47 +0100

    Make scripts run!!!!!

--- src/lib/lmixer.remote.ts
+++ src/lib/lmixer.remote.ts
@@ -1,7 +1,12 @@
 import { command } from "$app/server";
 import * as vb from "valibot";
+import { mqttClient } from "./server/mqtt";
 
 export const runScript = command(vb.string(), async (scriptPath) => {
-    // TODO: Write next SMN monday when I am actually in kistan
+    if (!mqttClient.connected) {
+        console.error("MQTT client is not connected. Cannot run scripts.");
+        return;
+    }
     console.log("Running script:", scriptPath);
+    mqttClient.publish("light_mixer/code/startScript", scriptPath);
 });
