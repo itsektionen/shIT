d2c2b4ed593f66c4e16a841b6444b7b357ec0ecd
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2026-04-26 17:33:26 +0200

    Improve dark colours

--- src/lib/components/ColorSelector.svelte
+++ src/lib/components/ColorSelector.svelte
@@ -4,12 +4,16 @@
 
     let { field }: { field: RemoteFormField<string> } = $props();
 
-    const NULL_COLOR = "";
-    const DEFAULT_COLOR = "#000000";
+    type HEX = `#${string}`;
+
+    const NULL_COLOR = "" as const;
+    const DEFAULT_COLOR: HEX = "#000000";
 
     const colors = [
         ["#000000", "Black"],
-        ["#777777", "Grey"],
+        ["#222222", "Dark Grey"],
+        ["#636363", "Grey"],
+        ["#aeaeae", "Light Grey"],
         ["#ffffff", "White"],
         [],
         ["#ff6467", "Red"],
@@ -22,15 +26,19 @@
         ["#cc99ff", "Laserviolet"],
         ["#e83d84", "Rosa"],
         [],
-        ["#9f0712", "Dark Red"],
+        // Dark colors are calculated by:
+        // oklch(from color-mix(in oklab, <color1>, oklch(0.4 0.25 270) 10%) <color1 OKLCH lightness> c h)
+        // Where color1 is the original dark color we stole from tailwind.
+        // The specific blue colour and the 10% factor are completely arbitrary
+        ["#9d0137", "Dark Red"],
         ["#973c00", "Dark Orange (aka brown)"],
-        ["#894b00", "Dark Yellow"],
-        ["#3c6300", "Dark Lime"],
-        ["#016630", "Dark Green"],
-        ["#005f5a", "Dark Teal"],
+        ["#805100", "Dark Yellow"],
+        ["#0b682b", "Dark Lime"],
+        ["#00694e", "Dark Green"],
+        ["#005e61", "Dark Teal"],
         ["#00598a", "Dark Sky"],
         ["#653290", "Dark Laserviolet"],
-        ["#8f0045", "Dark Rosa"],
+        ["#8c044f", "Dark Rosa"],
         [],
         ["#cc99ff", "Laserviolet"],
         ["#44687d", "Silicone blue"],
@@ -39,7 +47,19 @@
         ["#004791", "KTH-Blue"],
         ["#45b8da", "THS-Blue"],
         ["#a2ee8d", "TBas Green"],
-    ] as const;
+    ] satisfies ([HEX, string] | [])[];
+
+    /*[
+        oklch(from color-mix(in oklab, oklch(44.4% 0.177 26.899), oklch(0.4 0.25 270) 15%) 44.4% 0.177 h)
+        oklch(from color-mix(in oklab, oklch(47.0% 0.157 37.304), oklch(0.4 0.25 270) 15%) 47.0% 0.157 h)
+        oklch(from color-mix(in oklab, oklch(47.6% 0.114 61.907), oklch(0.4 0.25 270) 15%) 47.6% 0.114 h)
+        oklch(from color-mix(in oklab, oklch(45.3% 0.124 130.933), oklch(0.4 0.25 270) 15%) 45.3% 0.124 h)
+        oklch(from color-mix(in oklab, oklch(44.8% 0.119 151.328), oklch(0.4 0.25 270) 15%) 44.8% 0.119 h)
+        oklch(from color-mix(in oklab, oklch(43.7% 0.078 188.216), oklch(0.4 0.25 270) 15%) 43.7% 0.078 h)
+        oklch(from color-mix(in oklab, oklch(44.3% 0.11 240.79), oklch(0.4 0.25 270) 15%) 44.3% 0.11 h)
+        oklch(from color-mix(in oklab, oklch(43.8% 0.1502 306.19), oklch(0.4 0.25 270) 15%) 43.8% 0.1502 h)
+        oklch(from color-mix(in oklab, oklch(41.95% 0.169271 2.4025), oklch(0.4 0.25 270) 15%) 41.95% 0.169271 h)
+    ]*/
 </script>
 
 <div class="flex flex-wrap">
